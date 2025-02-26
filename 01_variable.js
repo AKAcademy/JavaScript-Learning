9de625f7d44eef6c116dc18ab8967ebd0c19fd51
Abhay chaudhary <142333047+AKAcademy@users.noreply.github.com>
2025-02-26 15:42:09 +0000

    Variable is Final

---
+++ 01_variable.js
@@ -0,0 +1,36 @@
+const accountId= 45  
+
+let accountEmail= "Abhay@gmail.com"
+var accountPassword="7265282"
+accountCity= "Lucknow"
+  
+
+// Now check the which is changeable which is not changeable 
+
+
+//accountId=44 // it is not allowed 
+
+accountEmail="Abhay2@gmail.com"//It is possible to change the let variable element
+
+console.log(accountEmail);
+
+accountPassword=6546948
+console.log(accountPassword);//User are able to change the Password in the var variable
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+//For decaleare the variable their are tow type one is let
+//  and second is var but we use only let because 
+
+
+
+
+
+
+
+
+
+
+
+console.log(accountId);
