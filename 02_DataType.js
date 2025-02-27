97f8466b846ced0d582be4cff82964bd04c59f90
Abhay chaudhary <142333047+AKAcademy@users.noreply.github.com>
2025-02-27 14:18:04 +0000

    dataType

---
+++ 02_DataType.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all js code as newest version
+
+//alert(5+6)  // we are using nodejs not browser
+
+console.log(3 
+    
+    + 3);  // code readibility is not good prefer good way 
+
+
+console.log("Abhay");
+
+
+let name ="Abhay"
+let age=20
+let isLoggedIn= false
+
+
+//number => 2 to power 53 
+//bigint
+//string=>""
+//boolean=> true/false
+//null=> Standalone value
+//undefined=>
+//symbol=>
+
+
+
+//Object 
+
+console.log(typeof"Abhay"); // String 
+
+
+console.log(typeof null);//object
+
+
+
+
