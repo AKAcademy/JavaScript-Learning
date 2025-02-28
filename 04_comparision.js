1a0ef921ec395a4dffdb490b0e8deb13a73ce888
Abhay chaudhary <142333047+AKAcademy@users.noreply.github.com>
2025-02-28 18:16:55 +0000

    Comparison

---
+++ 04_comparision.js
@@ -0,0 +1,19 @@
+
+
+
+console.log(2>1);
+console.log(2<=3);
+console.log(2==4);
+console.log(2 != 5);
+
+
+         //**********************   ===    ***********************//
+
+         console.log("2"==2); //Ans ==> True
+
+         console.log("2"===2);  // Ans ==> False
+
+
+
+         
+         
