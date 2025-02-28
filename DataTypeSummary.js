0815ccfa36c88d600dbd44b910d68da63e6e1597
Abhay chaudhary <142333047+AKAcademy@users.noreply.github.com>
2025-02-28 18:44:43 +0000

    dataTypeSummary

---
+++ DataTypeSummary.js
@@ -0,0 +1,68 @@
+// PRIMITIVE DATA TYPE
+// 7 TPYE
+
+//String : call by value
+//number
+//boolean
+//null
+//undefined
+//symbol
+//bogint
+
+const score = 100
+
+const scorevalue =100.3
+
+
+const isloggedIn = false
+
+const outlogged = null
+
+let userEmail = "bhay@gmail.com"
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+
+console.log(id===anotherId);
+
+
+const bignumber = 65895654964584n
+console.log(bignumber);
+
+
+
+
+// ################# Reference (Non prinmitive data type)  #########
+
+
+//Array , object , Function
+
+const heros = ["Shaktiman", "nagraj","doga"]
+{
+
+    name:"Abhay"
+    age:23
+
+}
+
+
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof userEmail);
+
+
+
+
+
+
+
+
+
+
+
+
