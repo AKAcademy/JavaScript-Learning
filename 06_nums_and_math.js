2d27dcf6149fcc0caea888b83ae7e703b1b3e34e
Abhay chaudhary <142333047+AKAcademy@users.noreply.github.com>
2025-03-02 10:36:42 +0000

    Number and Math

---
+++ 06_nums_and_math.js
@@ -0,0 +1,45 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+
+
+//****************************** ---- Math----*****************************
+
+
+console.log(Math.abs(4));
+
+
+console.log(Math.round(654.54));
+
+
+console.log(Math.ceil(4.2));
+
+
+console.log(Math.floor(6.9));
+
+console.log(Math.max(65,5,456,54,54,52));
+
+
+
+console.log(Math.random());
+
+console.log((Math.random()*10)+1)
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 20
+
+const max = 40
+
+console.log(Math.floor(Math.random()*(max-min + 1))+min);
+
+// it generate the random number between max number and min number 
+
