096bf6d90b731f1a38ae6aa80b051db9ba994302
Abhay chaudhary <142333047+AKAcademy@users.noreply.github.com>
2025-03-02 10:56:15 +0000

    Date and Time

---
+++ 07_Date_In_Js.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log( typeof myDate);
+
+
+
+let MycreatedDate = new Date( 2023 , 5 , 23)
+console.log(MycreatedDate.toDateString());
+console.log(MycreatedDate.toLocaleDateString());
+
+
+
+let mystamp = Date.now()
+console.log(mystamp)
