f41e0cb8b36b9b3cc75e762847f17451cb730e75
Abhay chaudhary <142333047+AKAcademy@users.noreply.github.com>
2025-03-05 16:16:14 +0000

    Array

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -1,1 +1,53 @@
-KeyboardEvent
+//Arrays 
+
+
+const myArray =  [1,2,32,3,1,5,5,"Abhay"]
+// console.log(myArray[2]);
+
+// const MyHeros = ["Shaktiman","Army","Abhay"]
+
+// console.log(myArray[7] + " Is the real " + MyHeros[1]);
+
+
+                   // Array Method 
+    // **************PUSH OPEATION*****************************
+// myArray.push(46)
+// console.log(myArray);
+
+    // **************POP OPEATION*****************************/
+
+// myArray.pop(5)
+// console.log(myArray);
+
+    // **************UNSHIFT OPEATION*****************************/
+
+// myArray.unshift(56)
+// console.log(myArray);
+
+    // **************shift OPEATION*****************************/
+
+// myArray.shift()
+// console.log(myArray);
+
+// console.log(myArray.includes(5));
+// console.log(myArray.indexOf(3));
+   
+// console.log(myArray);
+   
+// Slice , splice
+
+// console.log("A",myArray);
+// const My1 = myArray.slice(1,3)
+
+// console.log(My1);
+// console.log("B" , myArray);
+
+// const my2 = myArray.splice(1,3)
+// console.log("c",myArray);
+// console.log(my2);
+
+
+
+
+   
+
